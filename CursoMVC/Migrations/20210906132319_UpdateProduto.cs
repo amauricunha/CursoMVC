c9c5e0f824f5433d45933bd1636ed36d7525f86a
Amauri Cunha <proamauri@gmail.com>
2021-09-07 00:09:03 -0300

    first commit

---
+++ CursoMVC/Migrations/20210906132319_UpdateProduto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class UpdateProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
+        public string Description { get; set; }
+        [Display(Name = "Quantidade")]
+        [Range(0,2000, ErrorMessage ="Valor fora da faixa.")]
+        public int Amount { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+
+    }
+}
